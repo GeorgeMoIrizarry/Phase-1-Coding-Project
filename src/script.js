57e3d74cf9261a2a104be4b6c548cc60554934be
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-04-15 17:00:27 -0400

    added saiyan font href to head

--- src/script.js
+++ src/script.js
@@ -2,11 +2,11 @@ const fighterCards = document.querySelectorAll(".fighter-images")
 function fighterCard() {
     fighterCards.forEach(element => {
         element.addEventListener('mouseover', () => {
-            element.style.border = `1px solid yellow`
-        } )
+            element.style.border = '1px solid yellow'
+            })
         element.addEventListener('mouseout', () => {
             element.style.border = `none`
-        } )
+        })
     });
 }
 fighterCard()
