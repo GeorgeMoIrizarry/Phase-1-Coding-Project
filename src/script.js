6d8d6160567f1048f7e1d2b4783e08df739e45bc
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-04-18 12:06:48 -0400

    modify javascript

--- src/script.js
+++ src/script.js
@@ -131,7 +131,7 @@ function kiBlastOne(i, oppPlayerStats, name1, name2) {
 }
 function kamahamahaWaveOne(i, oppPlayerStats, name1, name2) { 
     let random = Math.floor(Math.random() * 1000)
-    let punchMulti = random * 90
+    let punchMulti = random * 120
     let newStats = oppPlayerStats.textContent
     oppPlayerStats.textContent = parseInt(newStats, 10) - punchMulti
     if (i == 1){
